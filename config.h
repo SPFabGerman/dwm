f9044186921c0b231147e509502f15d25527f7cd
Fabian Lindner <fabian@FabiansArch.localdomain>
2020-03-30 12:41:25 +0200

    Added Color to not focused window borders

--- config.h
+++ config.h
@@ -12,11 +12,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#519fdf";
+static const char col_green[]     = "#88b369";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_green },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
